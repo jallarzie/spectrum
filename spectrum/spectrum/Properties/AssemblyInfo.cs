353c9a914257830b7cd489db838be24cc0a66e05
Audrey Paiement <jallarzie@gmail.com>
2012-11-29 16:52:31 -0500

    Capitalised title in Assembly Info

--- spectrum/spectrum/Properties/AssemblyInfo.cs
+++ spectrum/spectrum/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("spectrum")]
-[assembly: AssemblyProduct("spectrum")]
+[assembly: AssemblyTitle("Spectrum")]
+[assembly: AssemblyProduct("Spectrum")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
